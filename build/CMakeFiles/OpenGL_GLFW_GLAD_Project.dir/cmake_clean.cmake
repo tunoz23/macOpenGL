97dd857df7c4f346defeb7b86ad33bf2bd81eaa1
tunaozturk <tunoz.23@yahoo.com>
2024-10-19 17:46:05 +0300

    initial commit for macOS

---
+++ build/CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/src/glad.c.o"
+  "CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/src/glad.c.o.d"
+  "CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/src/main.cpp.o.d"
+  "OpenGL_GLFW_GLAD_Project"
+  "OpenGL_GLFW_GLAD_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OpenGL_GLFW_GLAD_Project.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for OpenGL_GLFW_GLAD_Project.
+# This may be replaced when dependencies are built.
